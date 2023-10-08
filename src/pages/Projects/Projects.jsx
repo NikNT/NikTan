cce00ac59d99891023188d7a1248c03f54c6b7bb
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-08 13:07:31 -0400

    13. Fourth Card Added | Flex in AboutMe Fixed

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,14 +3,16 @@ import styles from "./Projects.module.css";
 
 const Projects = () => {
   return (
-    <div className={styles.container}>
-      <div className="heading">
-        <span>My projects</span>
+    <>
+      <div className={styles.container}>
+        <div className="heading">
+          <span>My projects</span>
+        </div>
+        <div className={styles.project_cards}>
+          <Card />
+        </div>
       </div>
-      <div className={styles.project_cards}>
-        <Card />
-      </div>
-    </div>
+    </>
   );
 };
 
