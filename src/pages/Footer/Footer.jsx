fffa41ef69491395289c6c773596f9c3a7fc60d7
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-12 09:36:06 -0400

    17. Footer - Connected to Firebase

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -6,8 +6,34 @@ import linkedin from "../../assets/linkedin.png";
 import link from "../../assets/link.png";
 import avatar from "../../assets/avatar-2.svg";
 import send from "../../assets/send.png";
+import { useState } from "react";
+import { checkInput } from "../../utils/Validation/Validation";
+import { toast } from "react-toastify";
+import { addMessageToFirestore } from "../../utils/Firestore/Firestore";
+import { successNotification } from "../../utils/Notifications/Notifications";
 
 const Footer = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errorMessage = checkInput(name, email, message);
+    if (errorMessage) {
+      toast.error(errorMessage);
+      return;
+    }
+
+    addMessageToFirestore(
+      name,
+      email,
+      message,
+      setName,
+      setEmail,
+      setMessage,
+      successNotification
+    );
+  };
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
@@ -24,16 +50,32 @@ const Footer = () => {
             <p>Send a message to connect with me!</p>
           </p>
         </div>
-        <form className={styles.footer_form}>
+        <form className={styles.footer_form} onSubmit={handleSubmit}>
           <p>Get in Touch?</p>
           <div>
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div>
-            <input type="email" placeholder="Email" />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
-            <textarea cols="30" rows="10" placeholder="Message"></textarea>
+            <textarea
+              cols="30"
+              rows="10"
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </div>
           <button>
             Send <img src={send} alt="Send" className={styles.send} />{" "}
